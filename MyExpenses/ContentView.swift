a20e4e62a9c27e6c888c251ab9926f42f091b04d
Saydulayev <saydulayev.wien@gmail.com>
2023-09-14 18:42:22 +0200

    Added a function to display the total amount of expenses and much more...

--- MyExpenses/ContentView.swift
+++ MyExpenses/ContentView.swift
@@ -10,26 +10,57 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+
+    var totalExpense: Double {
+        expenses.items.map { $0.amount }.reduce(0, +)
+    }
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(expenses.items) { item in
-                    Text(item.name)
+            VStack {
+                List {
+                    ForEach(expenses.items) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: "USD"))
+                        }
+                    }
+                    .onDelete(perform: removeItems)
                 }
-                .onDelete(perform: removeItems)
+                
+                HStack {
+                    Text("Total:")
+                        .font(.headline)
+                        
+                    Spacer()
+                    Text(totalExpense, format: .currency(code: "USD"))
+                        .font(.headline)
+                        .foregroundColor(.green)
+                }
+                .underline()
+                .padding()
             }
             .navigationTitle("MyExpenses")
             .toolbar {
                 Button {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 } label: {
                     Image(systemName: "plus")
                 }
             }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
         }
     }
+    
     func removeItems(at offsets: IndexSet) {
         expenses.items.remove(atOffsets: offsets)
     }
